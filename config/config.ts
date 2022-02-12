7648d90cc8facf14c15b447dc9b416dbcf1f1987
deger4beger <deger.begerrr@gmail.com>
2022-02-12 19:29:57 +0600

    setting to .env

--- config/config.ts
+++ config/config.ts
@@ -1,7 +1,7 @@
-export const NODE_PORT = process.env.PORT || 3030
-export const JWT_SECRET = process.env.JWT_SECRET || "ergrtogmhjoijn546oihj54u66456t4yuf6ty456o54nm6lk4j56"
-export const PG_HOST = process.env.PG_HOST || "localhost"
-export const PG_PORT = process.env.PG_PORT || 3060
-export const PG_USERNAME = process.env.PG_USERNAME || "postgres"
-export const PG_PASSWORD = process.env.PG_PASSWORD || "deger"
-export const PG_DB = process.env.PG_DB || "unknown_db"
+export const NODE_PORT = process.env.PORT || process.env.NODE_PORT
+export const JWT_SECRET = process.env.JWT_SECRET
+export const PG_HOST = process.env.PG_HOST
+export const PG_PORT = process.env.PG_PORT
+export const PG_USERNAME = process.env.PG_USERNAME
+export const PG_PASSWORD = process.env.PG_PASSWORD
+export const PG_DB = process.env.PG_DB
